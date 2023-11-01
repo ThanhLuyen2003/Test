3e620ec8073df4a245a38f75f24838461431a3ba
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-11-01 08:41:47 +0700

    o

--- app/src/main/java/com/example/thread/o.java
+++ app/src/main/java/com/example/thread/o.java
@@ -1,4 +1,13 @@
 package com.example.thread;
 
 public class o {
+    String abc;
+
+    public String getAbc() {
+        return abc;
+    }
+
+    public void setAbc(String abc) {
+        this.abc = abc;
+    }
 }
